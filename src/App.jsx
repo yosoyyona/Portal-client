da9b59bcea138dce367438a59eb02650d2a22fc3
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-05-04 15:27:46 +0200

    components-Error, Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@ import './App.css'
 import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 
+import Homepage from './pages/Homepage'
+import Navbar from './components/Navbar';
+import Error from './components/Error'
+
 function App() {
   
+  const [error, setError] = useState(null)
+
   return (
     <div className="App">
     
+      <Navbar />
+
+      <Routes>
+          <Route path={`/`} element={<Homepage />} />
+      </Routes>
+
+      {
+        error && <Error error={error} />
+      }
+
     </div>
   )
 }
---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error({error}) {
+  return (
+    <div className='error-message'>{error}</div>
+  )
+}
+
+export default Error
