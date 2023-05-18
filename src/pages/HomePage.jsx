8c4033ef177f951fa846bf14c5af8cd77f3d003e
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-05-18 22:37:53 +0200

    quiz - WIP

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,4 @@
-import { useState, useContext } from "react";
-import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/auth.context";
 import { Button, Pane, majorScale } from 'evergreen-ui'
 
 
