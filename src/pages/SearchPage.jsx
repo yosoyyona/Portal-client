24c0962a175f4b45f785240a223f30c17bf84a88
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-05-20 16:48:04 +0200

    search page - WIP, mongoose error

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,25 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useSearchParams } from "react-router-dom";
+import { Pane, SearchInput } from 'evergreen-ui'
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
 
 const SearchPage = () => {
 
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [results, setResults] = useState([])
 
-  const keyword = searchParams.get("keyword");
-  
-  // select between post, quiz, user
-  const type = searchParams.get("type"); 
-  
+  let [searchParams, setSearchParams] = useSearchParams();
+  let [keyword, setKeyword] = useState(searchParams.get("keyword") || "");
 
-  function handleParams(event) {
-    event.preventDefault();
-    
-    let params = serializeFormQuery(event.target);
-    setSearchParams(params);
+  const storedToken = localStorage.getItem('authToken');
+
+  const handleKeyword = e => {
+    setSearchParams({keyword: e.target.value})
+    setKeyword(e.target.value)
   }
 
+  const getResult = () => {
+    axios.get(`${API_URL}/search`,
+      { headers: { Authorization: `Bearer ${storedToken}` } }
+    )
+    .then((response) => {setResults(response.data)
+    console.log(response.data)})
+    .catch((error) => console.log(error))
+  }
+
+  useEffect(() => {
+    getResult()
+  }, [])
+
   return (
-    <div>SearchPage</div>
+    <div>
+      <form role="search">
+        <SearchInput type="text" value={keyword} onChange={handleKeyword} placeholder="Enter a keyword" />
+      </form>
+      
+      {/* <h4>Searched result with "{keyword}"</h4> */}
+
+      {results &&
+        <div id="result-list">
+        <p>result</p>
+          {/* {results.map(result => <p>{result}</p>)} */}
+        </div>
+      }
+      
+    </div>
   )
 }
 
