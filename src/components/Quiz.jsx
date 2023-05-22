b33c473f7cf5be74cb4395e04101257c361ae539
ApollineStev <apollinecanard@gmail.com>
2023-05-22 14:13:25 +0200

    answer in random order

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import { Pane, Button, Alert } from 'evergreen-ui'
 
 const Quiz = ({quiz}) => {
+
+  // active to change color depending on good bad answer
+  const [active, setActive] = useState(false)
+
+  
+
+  // to make an array of answer
+  const [answerList, setAnswerList] = useState([])
+  
+  useEffect(() => {
+
+     const answerArray = [...[quiz.answer,quiz.answer2, quiz.answer3, quiz.answer4]]
+     answerArray.sort(() => Math.random() - 0.5)
+     setAnswerList(answerArray)
+
+    console.log(answerArray, quiz.answer)
+  },[])
+
+
+  
+
+
+
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
     
     <div className='border'>
       <h3>{quiz.question}</h3>
       <p>{quiz.genre} </p>
       <p>{quiz.difficulty} </p>
-      
-      <div>
+      {answerList.map(answer => {
+        return(
+          <div style={{display:'flex', flexDirection:'row'}}>
+            <Button type="submit" 
+               /*onClick={quiz.answer === answer ? alert("Good answer! Try again") : alert("Wrong answer! Try again")}*/>{answer} </Button>
+          </div>
+        )
+      })}
+
+      {/*<div>
         <Button type="submit" marginRight={3} 
         onClick={() => alert("Correct answer!")}>{quiz.answer} </Button>
         
@@ -20,7 +56,7 @@ const Quiz = ({quiz}) => {
         onClick={() => alert("Wrong answer! Try again")}>{quiz.answer3} </Button>
         <Button type="submit" 
         onClick={() => alert("Wrong answer! Try again")}>{quiz.answer4} </Button>
-      </div>
+      </div>*/}
     </div>
 
   )
