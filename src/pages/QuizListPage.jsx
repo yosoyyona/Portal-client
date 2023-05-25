47fb098b3e53597c41b0ac52be6755b5efc4c1a7
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-05-25 21:23:33 +0200

    quiz list page-pagination

--- src/pages/QuizListPage.jsx
+++ src/pages/QuizListPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { AuthContext } from "../context/auth.context";
 import Quiz from '../components/Quiz'
-import { Pane, Button } from 'evergreen-ui'
+import { Heading, Pane, Button } from 'evergreen-ui'
 import axios from 'axios'
 
 function QuizListPage() {
@@ -21,6 +21,39 @@ function QuizListPage() {
     .catch((error) => console.log(error))
   }, [])
 
+  const [page, setPage] = useState(1)
+  const [filteredQuizzes, setFilteredQuizzes] = useState(quizzes)
+  const [backButtonStyle, setBackButtonStyle] = useState("")
+  const [nextButtonStyle, setNextButtonStyle] = useState("")
+
+  const displayButtons = (backButton, nextButton) => {
+    if (backButton) setBackButtonStyle("visible")
+    else setBackButtonStyle("disabled")
+    if (nextButton) setNextButtonStyle("visible")
+    else setNextButtonStyle("disabled")
+  }
+
+  const setCurrentPage = currentPage => {
+
+    if (quizzes.length <= 5) displayButtons(false, false)
+    else {
+      setPage(currentPage)
+
+      if (currentPage == 1) displayButtons(false, true)
+      else if (currentPage * 5 > quizzes.length) displayButtons(true, false)
+      else displayButtons(true, true) 
+    }
+  }
+
+  useEffect(() => {
+    setCurrentPage(page)
+  }, [])
+
+  useEffect(() => {
+    setFilteredQuizzes(pagination(page))
+  }, [page])
+
+  const pagination = page => quizzes.slice((page - 1) * 5, 5 * page)
 
   return (
     <div>
@@ -35,9 +68,24 @@ function QuizListPage() {
         </Pane>
       </Pane>
       
-      <div id="quiz-list" >
-        {quizzes.map(quiz=> <Quiz key={quiz._id} quiz={quiz} />)}
-      </div>
+      {
+        filteredQuizzes.length > 0 ?
+        <div id="quiz-list">
+          {filteredQuizzes.map(quiz => <Quiz key={quiz._id} quiz={quiz} />)}
+        </div>
+        : <Heading size={700}>No Quizzes present!</Heading>
+      }
+
+      {
+        quizzes.length > 5 &&
+        <div>
+          <Heading size={400} marginBottom="10px">Page {page}</Heading>
+        </div>
+      }
+
+      <Button className={backButtonStyle} onClick={() => { setCurrentPage(page - 1) }} size="small" marginRight="8px">Back</Button>
+      <Button className={nextButtonStyle} onClick={() => { setCurrentPage(page + 1) }} size="small">Next</Button>
+
     </div>
   )
 }
